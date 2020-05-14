8f7ffb59cc9bb772a613d99b296a09c5c770d90d
Aleksey Belchenkov <u608110@gmail.com>
2020-05-14 14:43:04 +0300

    Generate Recipes Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'recipes',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'recipes',
+    loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
   },
 ];
 
