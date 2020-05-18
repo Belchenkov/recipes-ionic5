40e5670fe2a4827bbf1f375184bac2bb2c9e3f08
Aleksey Belchenkov <u608110@gmail.com>
2020-05-18 14:59:05 +0300

    Display List Recipe

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,11 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './recipe.model';
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Steak',
+      imageUrl: 'https://i2.wp.com/www.foodrepublic.com/wp-content/uploads/2012/05/testkitchen_argentinesteak.jpg?resize=1280%2C%20560&ssl=1',
+      ingredients: [
+          'Meal',
+          'Souse',
+          'Salad'
+      ]
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://www.kitchensanctuary.com/wp-content/uploads/2019/09/Spaghetti-Bolognese-square-FS-0204.jpg',
+      ingredients: [
+          'Spaghetti',
+          'Tomatoes',
+          'Cheese'
+      ]
+    }
+  ];
 
   constructor() { }
 
