53d1a61b9bfe7da8d47aaa6ba7f7a0ede6bdde68
Aleksey Belchenkov <u608110@gmail.com>
2020-05-19 15:08:56 +0300

    State Recipe to Service

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
@@ -8,32 +9,12 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-  recipes: Recipe[] = [
-    {
-      id: 'r1',
-      title: 'Steak',
-      imageUrl: 'https://i2.wp.com/www.foodrepublic.com/wp-content/uploads/2012/05/testkitchen_argentinesteak.jpg?resize=1280%2C%20560&ssl=1',
-      ingredients: [
-          'Meal',
-          'Souse',
-          'Salad'
-      ]
-    },
-    {
-      id: 'r2',
-      title: 'Spaghetti',
-      imageUrl: 'https://www.kitchensanctuary.com/wp-content/uploads/2019/09/Spaghetti-Bolognese-square-FS-0204.jpg',
-      ingredients: [
-          'Spaghetti',
-          'Tomatoes',
-          'Cheese'
-      ]
-    }
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
+      this.recipes = this.recipesService.getAllRecipes();
   }
 
 }
