83911304a7a54d18abf7ae25eb3b015f0936e812
Aleksey Belchenkov <u608110@gmail.com>
2020-05-21 14:38:13 +0300

    Apply AlertCtrl

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 import { RecipesService } from '../recipes.service';
 import { Recipe } from '../recipe.model';
@@ -15,7 +16,8 @@ export class RecipeDetailPage implements OnInit {
   constructor(
       private activatedRoute: ActivatedRoute,
       private recipesService: RecipesService,
-      private router: Router
+      private router: Router,
+      private alertCtrl: AlertController
   ) { }
 
   ngOnInit() {
@@ -30,8 +32,24 @@ export class RecipeDetailPage implements OnInit {
   }
 
   onDeleteRecipe() {
-    this.recipesService.deleteRecipe(this.loadedRecipe.id);
-    this.router.navigate(['/recipes']);
+    this.alertCtrl.create({
+      header: 'Are you sure?',
+      message: 'Do you really want to delete the recipe?',
+      buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          },
+          {
+            text: 'Delete',
+            handler: () => {
+              this.recipesService.deleteRecipe(this.loadedRecipe.id);
+              this.router.navigate(['/recipes']);
+            }
+          }
+      ]
+    }).then(alertEl => {
+      alertEl.present();
+    });
   }
-
 }
